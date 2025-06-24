dacb1f6f6ba5da97ca8098f8801ec62ed013127e
J2yun <alex68566856@gmail.com>
2025-06-25 03:23:52 +0900

    [feat] 메시지 payload에 eventType 추가

--- src/main/java/com/medinote/backend/global/aws/dto/MedinoteSqsMessage.java
+++ src/main/java/com/medinote/backend/global/aws/dto/MedinoteSqsMessage.java
@@ -1,8 +1,8 @@
 package com.medinote.backend.global.aws.dto;
 
-public record MedinoteSqsMessage(Long medinoteId, String s3Key)
+public record MedinoteSqsMessage(String eventType, Long medinoteId, String s3Key)
 {
     public static MedinoteSqsMessage of(Long medinoteId, String s3Key) {
-        return new MedinoteSqsMessage(medinoteId, s3Key);
+        return new MedinoteSqsMessage("MEDINOTE_TRANSFORM_REQUEST",medinoteId, s3Key);
     }
 }
